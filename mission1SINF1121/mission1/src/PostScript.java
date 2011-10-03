e9b6659371c31d3481ffc157d329d8c4d8fce8ab
Florentin Rochet <oversnals@hotmail.com>
2011-10-03 15:29:32 +0200

    rejout d'une hashMap

--- mission1SINF1121/mission1/src/PostScript.java
+++ mission1SINF1121/mission1/src/PostScript.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Stack;
 
 
@@ -7,10 +8,12 @@ public class PostScript implements PostScriptInterface {
 	
 	private String filePathIn;
 	private Stack<Double> stack;
+	private HashMap<String,Double> map;
 
 	public PostScript(String filePathIn){
 		this.filePathIn = filePathIn;
 		this.stack = new Stack<Double>();
+		map = new HashMap<String,Double>();
 	}
 	@Override
 	public void applyToken(String token) throws TokenException {
